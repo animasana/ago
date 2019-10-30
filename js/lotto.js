6fa927b94ffb08d9cc6bba349e13466b715fff0e
animasana <41289861+animasana@users.noreply.github.com>
2019-10-31 00:02:41 +0900

    Update lotto.js

--- js/lotto.js
+++ js/lotto.js
@@ -1,64 +1,73 @@
-function Lotto (nRows, nCols, nSize) {
-    if (nRows >= 1 && nCols >= 1 && nSize >= 1 && nCols <= nSize) {
-        this.nRows = nRows;
-        this.nCols = nCols;
-        this.nSize = nSize;
+function Lotto(nRows, nCols, nSize) {
+    this.nRows = nRows;
+    this.nCols = nCols;
+    this.nSize = nSize;
 
-        this.lottoMatrix = new Array(nRows);
-        for(let i = 0; i < nRows; i++)
-            this.lottoMatrix[i] = new Array(nCols);
+    this.matrix = new Array(nRows).fill();
+    for(let i = 0; i < nRows; i++)
+        this.matrix[i] = new Array(nCols).fill();
+}
+
+Lotto.prototype.sample = function() {
+    if (this.nRows > 0 && this.nCols > 0 && this.nSize >= this.nCols) {
+        for (let i = 0; i < this.matrix.length; i++) {
+            let initSeq = new Array(this.nSize)
+                .fill()
+                .map((_, idx) => idx + 1);                
+            for (let j = 0; j < this.matrix[i].length; j++) {
+                this.matrix[i][j] = initSeq.splice(Math.floor(initSeq.length * Math.random()), 1)[0];
+            }
+        }
     }
     else {
-        throw Error("Illegal Argument!!!");
+        throw Error("Invalid Argument!!!");
     }
 }
 
-Lotto.prototype.sample = function () {
-    const initSeq = [];
-
-    const reset = (arr) => {
-        for (let i = 0; i < this.nSize; i++) {
-            arr.push(i + 1);
-        }
-    }
-
-    for (let i = 0; i < this.lottoMatrix.length; i++) {
-        reset(initSeq);
-        for (let j = 0; j < this.lottoMatrix[i].length; j++)
-            this.lottoMatrix[i][j] = initSeq.splice(Math.floor(initSeq.length * Math.random()), 1);
-        initSeq.length = 0;
-    }
-}
-
-Lotto.prototype.sortMatrix = function () {
-    this.lottoMatrix.forEach(function(item) {
-        item.sort(function (first, second) {
+Lotto.prototype.sort = function() {
+    for (const aRow of this.matrix) {
+        aRow.sort((first, second) => {
             return first - second;
-        });
-    });
-}
-
-Lotto.prototype.show = function (id) {
-    this.sample();
-    this.sortMatrix();
-    this.printToBrowser(id);
-}
-
-Lotto.prototype.pad = function (n, fig) {
-    n = n + "";
-    return n.length >= fig ? n : new Array(fig - n.length + 1).join("0") + n;
-}
-
-Lotto.prototype.printToBrowser = function (id) {
-    let output = "";
-
-    const fig = Math.floor(Math.log10(this.nSize)) + 1;
-    for (const aRow of this.lottoMatrix) {
-        for (const item of aRow) {
-            output += this.pad(item, fig) + " ";        
-        }
-        output += "<br />";
+        });        
     }    
-    output += "<br />";    
-    document.getElementById(id).innerHTML += output;
+}
+
+Lotto.prototype.print = function(disp) {    
+    const fig = Math.floor(Math.log10(this.nSize)) + 1;
+    const pad = (n, fig) => {
+        n = n + '';
+        return n.length >= fig ? n : new Array(fig - n.length + 1).join('0') + n;
+    }   
+    
+    disp.innerHTML = '';
+    this.matrix.forEach(aRow => {
+        aRow.forEach(element => {            
+            disp.innerHTML += pad(element, fig) + ' ';
+        });
+        disp.innerHTML += '<br>';
+    });
+    disp.innerHTML += '<br>';    
+}
+
+Lotto.prototype.show = function(disp) {
+    try {
+        this.sample()
+        this.sort()
+        this.print(disp)
+    }
+    catch (e) {
+        this.logErrors(e, disp);
+    }
+}
+
+Lotto.prototype.logErrors = function(e, disp) {
+    disp.innerHTML += e;
+    disp.innerHTML += '<br><br>';
+
+    console.log(e);
+    console.log('\n');
+}
+
+Lotto.prototype.logB = function(base, x) {
+    return Math.log(x) / Math.log(base);    
 }
